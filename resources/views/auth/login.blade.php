c207333a49c812cdb0b0bf1dafe78fb43934c4f7
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-03-18 10:13:17 +0100

    login implemented

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -8,7 +8,8 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -16,10 +17,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -27,14 +26,17 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                    name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
@@ -44,4 +46,166 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!--plugins-->
+    <link href="../dashboard/assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+    <link href="../dashboard/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+    <link href="../dashboard/assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <!-- loader-->
+    <link href="../dashboard/assets/css/pace.min.css" rel="stylesheet" />
+    <script src="../dashboard/assets/js/pace.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="../dashboard/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../dashboard/assets/css/bootstrap-extended.css" rel="stylesheet">
+    <link href="../dashboard/https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+        rel="stylesheet">
+    <link href="../dashboard/assets/css/app.css" rel="stylesheet">
+    <link href="../dashboard/assets/css/icons.css" rel="stylesheet">
+    <title>Rocker - Bootstrap 5 Admin Dashboard Template</title>
+</head>
+
+<body class="">
+    <!--wrapper-->
+    <div class="wrapper">
+        <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+            <div class="container">
+                <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+                    <div class="col mx-auto">
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <div class="p-4">
+                                    <div class="mb-3 text-center">
+                                        <img src="assets/images/logo-icon.png" width="60" alt="" />
+                                    </div>
+                                    <div class="text-center mb-4">
+                                        @include('notifications.flash-messages')
+                                        <h5 class="">Rocker Admin</h5>
+                                        <p class="mb-0">Please log in to your account</p>
+                                    </div>
+                                    <div class="form-body">
+                                        <form action="{{ route('login') }}" method="POST" class="row g-3">
+                                            @csrf
+                                            <div class="col-12">
+                                                <label for="inputEmailAddress" class="form-label">Email</label>
+                                                <input type="email" name="email"
+                                                    class="form-control @error('email') is-invalid @enderror" required
+                                                    autocomplete="email" value="{{ old('email') }}"
+                                                    id="inputEmailAddress" placeholder="Enter your email address">
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            <div class="col-12">
+                                                <label for="inputChoosePassword" class="form-label">Password</label>
+                                                <div class="input-group" id="show_hide_password">
+                                                    <input type="password" name="password"
+                                                        class="form-control border-end-0 @error('password') is-invalid @enderror"
+                                                        required value="{{ old('password') }}" id="inputChoosePassword"
+                                                        value="12345678" placeholder="Enter Password"> <a
+                                                        href="javascript:;" class="input-group-text bg-transparent"><i
+                                                            class='bx bx-hide'></i></a>
+
+                                                </div>
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-check form-switch">
+                                                    <input class="form-check-input" type="checkbox" id="remember_me">
+                                                    <label class="form-check-label"
+                                                        for="flexSwitchCheckChecked">Remember Me</label>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6 text-end">
+                                                @if (Route::has('password.request'))
+                                                    <a href="authentication-forgot-password.html">Forgot Password ?</a>
+                                                @endif
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="d-grid">
+                                                    <button type="submit" class="btn btn-primary">Sign in</button>
+                                                </div>
+                                            </div>
+                                            <div class="col-12">
+                                                <div class="text-center ">
+                                                    <p class="mb-0">Don't have an account yet? <a
+                                                            href="authentication-signup.html">Sign up here</a>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="login-separater text-center mb-5"> <span>OR SIGN IN WITH</span>
+                                        <hr />
+                                    </div>
+                                    <div class="list-inline contacts-social text-center">
+                                        <a href="javascript:;"
+                                            class="list-inline-item bg-facebook text-white border-0 rounded-3"><i
+                                                class="bx bxl-facebook"></i></a>
+                                        <a href="javascript:;"
+                                            class="list-inline-item bg-twitter text-white border-0 rounded-3"><i
+                                                class="bx bxl-twitter"></i></a>
+                                        <a href="javascript:;"
+                                            class="list-inline-item bg-google text-white border-0 rounded-3"><i
+                                                class="bx bxl-google"></i></a>
+                                        <a href="javascript:;"
+                                            class="list-inline-item bg-linkedin text-white border-0 rounded-3"><i
+                                                class="bx bxl-linkedin"></i></a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--end row-->
+            </div>
+        </div>
+    </div>
+    <!--end wrapper-->
+    <!-- Bootstrap JS -->
+    <script src="../dashboard/assets/js/bootstrap.bundle.min.js"></script>
+    <!--plugins-->
+    <script src="../dashboard/assets/js/jquery.min.js"></script>
+    <script src="../dashboard/assets/plugins/simplebar/js/simplebar.min.js"></script>
+    <script src="../dashboard/assets/plugins/metismenu/js/metisMenu.min.js"></script>
+    <script src="../dashboard/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    <!--Password show & hide js -->
+    <script>
+        $(document).ready(function() {
+            $("#show_hide_password a").on('click', function(event) {
+                event.preventDefault();
+                if ($('#show_hide_password input').attr("type") == "text") {
+                    $('#show_hide_password input').attr('type', 'password');
+                    $('#show_hide_password i').addClass("bx-hide");
+                    $('#show_hide_password i').removeClass("bx-show");
+                } else if ($('#show_hide_password input').attr("type") == "password") {
+                    $('#show_hide_password input').attr('type', 'text');
+                    $('#show_hide_password i').removeClass("bx-hide");
+                    $('#show_hide_password i').addClass("bx-show");
+                }
+            });
+        });
+    </script>
+    <!--app JS-->
+    <script src="../dashboard/assets/js/app.js"></script>
+</body>
+
+</html>
